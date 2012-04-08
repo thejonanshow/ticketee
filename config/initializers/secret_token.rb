330a58a23f3844130248aba960453066b7d240e4
Jonan Scheffler <jonanscheffler@gmail.com>
2012-04-08 14:21:55 -0400

    Omnium rerum principia parva sunt.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = '65a2303f62202e6468313754a51d74a6fec83b70133eee98e9cd3df93c2b141f72fffe5b1249d510a0cb881c27bfd1887d105d04fafdb5c0d0ea99688e061f2d'
